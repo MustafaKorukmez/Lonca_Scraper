0acbde2d28b8494bfd8aff7a82e3c74b1d35fb28
m00878601 <mustafa.korukmez2@huawei.com>
2024-11-19 09:04:22 +0300

    Define fields in the Item model

--- lonca_scraper/lonca_scraper/items.py
+++ lonca_scraper/lonca_scraper/items.py
@@ -7,6 +7,22 @@ import scrapy
 
 
 class LoncaScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    _id = scrapy.Field()
+    stock_code = scrapy.Field()
+    color = scrapy.Field()
+    discounted_price = scrapy.Field()
+    images = scrapy.Field()
+    is_discounted = scrapy.Field()
+    name = scrapy.Field()
+    price = scrapy.Field()
+    price_unit = scrapy.Field()
+    product_type = scrapy.Field()
+    quantity = scrapy.Field()
+    sample_size = scrapy.Field()
+    series = scrapy.Field()
+    status = scrapy.Field()
+    fabric = scrapy.Field()
+    model_measurements = scrapy.Field()
+    product_measurements = scrapy.Field()
+    createdAt = scrapy.Field()
+    updatedAt = scrapy.Field()
