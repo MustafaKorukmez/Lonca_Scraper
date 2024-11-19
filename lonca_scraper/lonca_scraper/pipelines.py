c09926a0fd1e36fa4e8affa80d3f78e1737e6d54
m00878601 <mustafa.korukmez2@huawei.com>
2024-11-19 09:43:30 +0300

    Configure MongoDB connection in pipeline

--- lonca_scraper/lonca_scraper/pipelines.py
+++ lonca_scraper/lonca_scraper/pipelines.py
@@ -3,11 +3,25 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
+import pymongo
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
 class LoncaScraperPipeline:
+
+    def __init__(self):
+        self.mongo_uri = 'mongodb://localhost:27017'
+        self.mongo_db = 'lonca_database'
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
     def process_item(self, item, spider):
+        collection_name = 'products'
+        self.db[collection_name].insert_one(dict(item))
         return item
+
+    def close_spider(self, spider):
+        self.client.close()
